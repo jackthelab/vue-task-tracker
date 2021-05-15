869c37368a33bb7471071cb7992c6ab756e5af37
Jacob Hampton <jhampton792@gmail.com>
2021-05-15 16:24:05 -0500

    Added first custom component

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+
+</template>
+
+<script>
+
+  export default {
+    name: "Header",
+    // could write props a number of ways
+    // Array... props: ['title']
+    // objectOne... props: { title: String }
+    // objectTwo... below
+    props: {
+      title: {
+        type: String,
+        default: "Hello World"
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
