e03ddda6326c76bf2f2da79299076b7db0619efe
Jacob Hampton <jhampton792@gmail.com>
2021-05-15 17:24:47 -0500

    Added ability to delete activity

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Tasks @delete-task="deleteTask  " :tasks="tasks" />
   </div>
 </template>
 
@@ -21,6 +21,13 @@
         tasks: [
 
         ]
+      }
+    },
+    methods: {
+      deleteTask(id) {
+        if(confirm('Are you sure you want to delete this task?')) {
+          this.tasks = this.tasks.filter((task) => task.id !== id)
+        }
       }
     },
     created() {
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>
       {{ task.text }} 
-      <i class="fas fa-times"></i>
+      <i @click="deleteTask(task.id)" class="fas fa-times"></i>
       </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -13,6 +13,11 @@ export default {
   name: 'Task',
   props: {
     task: Object
+  },
+  methods: {
+    deleteTask(id) {
+      this.$emit('delete-task', id)
+    }
   }
 }
 </script>
